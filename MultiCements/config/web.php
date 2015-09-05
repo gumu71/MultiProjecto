2d06768a14d863682dfbe056d7f47d3c02c689a6
jhonhenry <jhonfer_1007@hotmail.com>
2015-09-05 12:01:16 -0400

    actualizando

--- MultiCements/config/web.php
+++ MultiCements/config/web.php
@@ -9,7 +9,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'MSam_FW-79PzKUfzlGlbtxuDK_WxyhLj',
+            'cookieValidationKey' => 'Tofql0SmirAg2EwxeW2Q96qr1rADNjwy',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
