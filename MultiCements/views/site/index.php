f6e558425c4bfb43b06108b71133d257e2e35b69
harrysito <harrypramos@gmail.com>
2015-09-05 12:02:54 -0400

    haciendo pruebas

--- MultiCements/views/site/index.php
+++ MultiCements/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Welcome to my life!</h1>
+        <h1>Welcome to my Dog!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
